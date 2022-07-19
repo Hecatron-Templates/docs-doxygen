35e1f4ce32e0e5a0ef032e2e8447062a6a722dcb
grbd <hecatrons.workshop@gmail.com>
2022-07-19 23:20:24 +0100

    updates

---
+++ docs/builders/1/run.py
@@ -0,0 +1,33 @@
+import sys
+from os import path
+from sphinx.cmd.build import main
+
+# Get a directory path relative to this script
+def get_path(dirpath):
+    scriptdir = path.dirname(__file__)
+    destpath = path.join(scriptdir, dirpath)
+    destpath = path.abspath(destpath)
+    return destpath
+
+# Setup sphinx options
+opts = []
+
+# Set for outputing to markdown
+opts += ['-b', 'markdown']
+
+# Set the cache directory to outside the build directory
+doctreesdir = get_path('doctrees')
+opts += ['-d', doctreesdir]
+
+# Pull conf.py from here instead of the source directory
+confpath = get_path('.')
+opts += ['-c', confpath]
+
+# Specify source and destination
+srcdir = get_path('source')
+builddir = get_path('build')
+opts += [srcdir, builddir]
+
+# Run Spinx
+#sys.exit(main(sys.argv[1:]))
+sys.exit(main(opts))
